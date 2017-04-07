7b18777479475fa99cbf70cb1f4fbb4ab0f880f8
Bobby Smith <bobby.smith@gmail.com>
2017-04-07 16:01:33 -0500

    add default region

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ EB_BUCKET=jackmallet-server
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name jackmallet-server \
+aws --region="us-east-1" elasticbeanstalk create-application-version --application-name jackmallet-server \
 	--version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
