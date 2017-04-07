b8a69aa43e5cf797a101b341c0a3d56fbbded75b
Bobby Smith <bobby.smith@gmail.com>
2017-04-07 16:08:13 -0500

    update region

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ docker push fabianwareio/jackmallet-server:$SHA1
 EB_BUCKET=jackmallet-server
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
-aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+aws --region="us-east-1" s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws --region="us-east-1" elasticbeanstalk create-application-version --application-name jackmallet-server \
 	--version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version
-aws elasticbeanstalk update-environment --environment-name jackmallet-server-prod \
+aws --region="us-east-1" elasticbeanstalk update-environment --environment-name jackmallet-server-prod \
 	--version-label $SHA1
